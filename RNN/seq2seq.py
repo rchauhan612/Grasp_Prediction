b793965f89d01ee6ed8b12164c7a3f01e1a77904
Ravi <raghur1@vt.edu>
2019-06-19 17:03:51 -0400

    classification 'works'. does not produce good results

--- RNN/seq2seq.py
+++ RNN/seq2seq.py
@@ -1,20 +1,29 @@
+# %% import everything
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 tf.enable_eager_execution(config = config)
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pickle
+# from tqdm import tqdm_notebook as tqdm
 from tqdm import tqdm
 
+# %%
 rnn_units = 200
-batch_size = 512
+batch_size = 1
 n_dims = 16
 
-test_groups = True
+test_groups = False
 num_cat = 8 if test_groups else 33
+train_split = 0.66
+
+# %%
 
 def gather_data(filename = 'all_data_seperate.pickle'):
     with open(filename, 'rb') as input_file:
@@ -28,7 +37,7 @@ def create_sequences(traj_data, labels, num_seqs = 5, train_split = 0.66):
     for trial, label in zip(traj_data, labels):
         inds = np.linspace(0, len(trial), num_seqs + 1)[1:].astype('int')
         for ind in inds:
-            seq_list.append(trial[:ind].astype('float32'))
+            seq_list.append(trial[:ind])
             temp = np.zeros(num_cat)
             temp[label-1] = 1
             label_list.append(temp)
@@ -46,68 +55,85 @@ def create_sequences(traj_data, labels, num_seqs = 5, train_split = 0.66):
     train_data = data.take(train_size).batch(batch_size, drop_remainder = True)
     test_data = data.skip(train_size)
 
-    return train_data, test_data
+    return train_data, test_data, len(seq_list)
+
+# %%
 
 def cce_loss(pred, truth, num_cat):
     cce = tf.keras.losses.CategoricalCrossentropy()
-
-    # truth_arr = np.zeros(batch_size, num_cat)
-    # truth_arr[truth-1] = 1
     return cce(pred, truth)
 
-def train_model(model, data, EPOCHS = 5, learning_rate = 0.001):
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    loss_hist = []
-    for epoch in range(EPOCHS):
-        print('Epoch: {}/{}'.format(epoch+1, EPOCHS))
-        hidden = model.reset_states()
-        for dat in tqdm(data):
-            traj = dat[0]
-            label = dat[1]
-            with tf.GradientTape() as tape:
-                current_loss = cce_loss(model(tf.cast(traj, dtype = tf.float32)), label, num_cat)
-
-            grads = tape.gradient(current_loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-    return loss_hist
-
-def create_model():
-    model = tf.keras.Sequential([
-        tf.keras.layers.CuDNNLSTM(units = rnn_units,
-                            batch_input_shape = (batch_size, None, n_dims),
-                            return_sequences = False,
-                            recurrent_initializer = 'glorot_uniform',
-                            stateful = True,
-                            dtype = 'float32'),
-        tf.keras.layers.Dense(num_cat, activation=tf.nn.softmax, dtype = 'float32')
-    ])
-
-    model.build((None, n_dims))
-    return model
+# %%
 
 def pred(model, traj, label):
     pred_class = np.amax(model(traj)) + 1
     print('True class: {}\t Predicted class: {}'.format(label, pred_class))
 
-if __name__ == '__main__':
-    trajs, labels = gather_data()
+# %%
 
-    if test_groups:
-        labels = [l[0] for l in labels]
-    else:
-        labels = [l[1] for l in labels]
+print('Collecting data...')
+trajs, labels = gather_data()
 
-    print('Forming dataset...')
-    train_data, test_data = create_sequences(trajs, labels)
+if test_groups:
+    labels = [l[0] for l in labels]
+else:
+    labels = [l[1] for l in labels]
 
-    print('Creating model...')
-    model = create_model()
-    print(model.summary())
-    input('Done')
+print('Forming dataset...')
+train_data, test_data, num_seqs = create_sequences(trajs, labels, num_seqs = 1)
 
-    print('Training model...')
-    loss_hist = train_model(model, train_data)
-    input('Done')
+# %%
 
-    plt.plot(loss_hist)
+print('Creating model...')
+
+model = tf.keras.Sequential([
+    tf.keras.layers.CuDNNLSTM(units = rnn_units,
+                        batch_input_shape = (batch_size, None, n_dims),
+                        return_sequences = True,
+                        recurrent_initializer = 'glorot_uniform',
+                        stateful = True,
+                        dtype = 'float32'),
+    tf.keras.layers.CuDNNLSTM(units = rnn_units,
+                        return_sequences = True,
+                        dtype = 'float32'),
+    tf.keras.layers.CuDNNLSTM(units = rnn_units,
+                        return_sequences = False,
+                        dtype = 'float32'),
+    tf.keras.layers.Dense(num_cat, activation=tf.nn.softmax, dtype = 'float32')
+])
+
+model.build((None, n_dims))
+print(model.summary())
+
+# %%
+
+print('Training model...')
+EPOCHS = 5
+learning_rate = 0.000001
+# optimizer = tf.train.AdagradOptimizer(learning_rate)
+# optimizer = tf.train.AdamOptimizer(learning_rate)
+optimizer = tf.train.RMSPropOptimizer(learning_rate)
+loss_hist = []
+for epoch in range(EPOCHS):
+    print('Epoch: {}/{}'.format(epoch+1, EPOCHS))
+    # hidden = model.reset_states()
+    for _, dat in tqdm(enumerate(train_data), total = int(train_split * num_seqs / batch_size)):
+        traj = tf.cast(dat[0], dtype = tf.float32)
+        label = dat[1]
+        with tf.GradientTape() as tape:
+            current_loss = cce_loss(model(traj),
+                                    label,
+                                    num_cat)
+
+        grads = tape.gradient(current_loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        loss_hist.append(current_loss)
+
+# %%
+plt.plot(loss_hist)
+plt.show()
+
+# %%
+
+print(len(loss_hist))
+print(loss_hist[0])
